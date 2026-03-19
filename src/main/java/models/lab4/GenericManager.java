9b88df6f8d046ffb57244050ce7c7fa1cd14e626
PC <PC@NVHThuong.lan>
2026-03-19 20:43:33 +0700

    lab4

---
+++ src/main/java/models/lab4/GenericManager.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models.lab4;
+
+/**
+ *
+ * @author PC
+ */
+import java.util.ArrayList;
+
+public class GenericManager<T> {
+    private ArrayList<T> list = new ArrayList<>();
+
+    public void add(T obj) {
+        list.add(obj);
+    }
+
+    public void showAll() {
+        for (T obj : list) {
+            System.out.println(obj.toString());
+        }
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+    public T getFirst() throws EmptyListException {
+        if (list.isEmpty()) throw new EmptyListException("Danh sách rỗng!");
+        return list.get(0);
+    }
+
+    public T getLast() throws EmptyListException {
+        if (list.isEmpty()) throw new EmptyListException("Danh sách rỗng!");
+        return list.get(list.size() - 1);
+    }
+}
